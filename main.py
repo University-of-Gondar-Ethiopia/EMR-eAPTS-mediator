0c003e05b75db0e879974d4220bf88eba4b8872f
Redet Getachew <redetg@gmail.com>
2024-07-22 12:15:18 +0300

    feat: adds test methods for openhim registration, authentication and heartbeat signal

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import requests
 import os
 from component.emr import EMR
 from component.eapts import EAPTS
+from component.openhim.auth import auth
+from component.openhim.heartbeat import Heartbeat
+from apscheduler.schedulers.background import BackgroundScheduler
+from component.openhim.openhim import Openhim
+
 
 app = FastAPI()
 load_dotenv()
 
 @app.get("/")
 def read_root():
-    return testAuthenticateEAPTS()
+    return testHeartbeat()
     return {"Hello": "World"}
 
 
@@ -28,4 +33,16 @@ def testAuthenticateEAPTS():
     eapts = EAPTS()
     return  eapts.authenticate();
 
-    
+def testOpenHIMauth():
+    auth2 = auth()
+    auth2.authenticate()
+    return auth2.gen_auth_headers()
+
+def testHeartbeat():
+    bgscheduler = BackgroundScheduler()
+    hb = Heartbeat(auth(), options = {"verify_cert": False,"urn":"urn:uuid:emr-eapts-interoperability new"}, scheduler = bgscheduler)
+    return hb.activate()
+
+def testOpenhimRegistration():
+    openhim = Openhim(options = {"verify_cert": False})
+    return openhim.register()
