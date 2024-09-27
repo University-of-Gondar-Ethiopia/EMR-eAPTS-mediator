cde239b570ae178e2dd9512ef77f8a8309ae3737
= <=>
2024-09-27 13:56:15 +0300

    Fix: Initialize authentication variable to None

--- app/component/eapts.py
+++ app/component/eapts.py
@@ -16,6 +16,7 @@ class EAPTS:
         self.DuItems_url = os.getenv("EAPTS_DUITEMS_ENDPOINT")
         self.DtpCase_url = os.getenv("EAPTS_DTPCASE_ENDPOINT")
         self.stockStatus_url = os.getenv("EAPTS_STOCK_STATUS_ENDPOINT")
+        self.auth = None
 
     def getAuthHeader(self):
         if self.auth is None:
